8acf433d8e2daa059b37549e3d7bf682ff161ef4
Pratik Makvana <161926450+Pratik-makvanaa@users.noreply.github.com>
2026-08-06 10:11:59 +0530

    Time: 0 ms (100%), Space: 42.7 MB (12.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -5,9 +5,11 @@ class Solution {
 
         while (n != 0) {
 
-            n = n & (n - 1);
+            if ((n & 1) == 1) {
+                count++;
+            }
 
-            count++;
+            n = n >>> 1;
         }
 
         return count;
